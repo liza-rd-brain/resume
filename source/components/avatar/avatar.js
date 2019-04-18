6e8060640bbd52e75c4f55941e4f7b8c762ae614
liza <lisaminakova@gmail.com>
2019-04-18 23:01:17 +0300

    настроены шаблоны html,pug

---
+++ source/components/avatar/avatar.js
@@ -0,0 +1,12 @@
+/* тест */
+
+export default function (array, className) {
+    var menu = document.createElement("ul");
+    menu.className = className;
+    var listItems = '';
+    array.forEach(function(item) {
+        listItems += '<li>' + item + '</li>';
+    });
+    menu.innerHTML = listItems;
+    return menu;
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  source: path.join(__dirname, "source"),
+  build: path.join(__dirname, "build")
+};
+
+module.exports = {
+  entry: PATHS.source + "/index.js",
+  output: {
+    path: PATHS.build,
+    filename: "[name].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PATHS.source + "/index.pug"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: {
+          pretty: true
+        }
+      }
+    ]
+  }
+};
